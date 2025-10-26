1909536cca3f7bb300ec979c653d7868bacff541
Adam Yoonas <57850400+Kingadamy@users.noreply.github.com>
2025-10-25 18:43:27 -0700

    update

--- main.cpp
+++ main.cpp
@@ -7,27 +7,47 @@ using namespace std;
 
 const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
 
-// create main menu function for these options:
-// *** GOAT MANAGER 3001 ***
-// [1] Add a goat
-// [2] Delete a goat
-// [3] List goats
-// [4] Quit
-// Choice --> 
 
-int main_menu() {
- cout << "*** GOAT MANAGER 3001 ***\n"
-      << "[1] Add a goat\n"
-      << "[2] Delete a goat\n"
-      << "[3] List goats\n"
-      << "[4] Quit\n"
-      << "Choice --> ";
+int select_goat(list<Goat> trip){
+    cout << "Which goat would you like to delete? (pick a number) \n";
+    int count = 1;
+    for(auto goat : trip) { // this loop goes through each goat in the list
+        cout << count++ << // display how many
+            " Name: " << goat.get_name() <<
+            ", Age: " << goat.get_age() <<
+            ", Color: " << goat.get_color() << endl;
+    }
 
-      int choice;
-        cin >> choice;
-        return choice;
+    int choice; // get user choice
+    cin >> choice;
+    return choice;
+
 }
-      
+
+void delete_goat(list<Goat> &trip) {
+    if(trip.empty()) { // check if the list is empty
+        cout << "No goats to delete.\n";
+        return;
+    }
+
+    int selection = select_goat(trip); // get the user's selection
+
+    // see if choice exists
+    if(selection < 1 || selection > trip.size()) {
+        cout << "Invalid selection. \n";
+        return;
+    }
+
+    // iterate to the selected goat
+    auto it = trip.begin();
+    advance(it, selection - 1); // go to position we want to delete
+
+    cout << "Deleted: " << it->get_name() << endl; // show which goat is deleted
+    trip.erase(it); // remove the goat from the list
+}
+                        
+
+
 void add_goat(list<Goat> &trip, string names[], string colors[]) { // create a new goat with random name, color, age
     int name_used = rand() % SZ_NAMES; // select random name from the names array
     int color_used = rand() % SZ_COLORS; // select random color from the colors array
@@ -44,7 +64,7 @@ void add_goat(list<Goat> &trip, string names[], string colors[]) { // create a n
          << ", Color: " << new_goat.get_color() << endl;
 }
 
-void display_trip(list<Goat> &trip) { // display all goats in the trip
+void display_trip(list<Goat> trip) { // display all goats in the trip
     if(trip.empty()) { // check if the list is empty
         cout << "No goats in the trip.\n";
         return;
@@ -61,13 +81,28 @@ void display_trip(list<Goat> &trip) { // display all goats in the trip
 }
 
 
+int main_menu() {
+ cout << "*** GOAT MANAGER 3001 ***\n"
+      << "[1] Add a goat\n"
+      << "[2] Delete a goat\n"
+      << "[3] List goats\n"
+      << "[4] Quit\n"
+      << "Choice --> ";
+
+      int choice;
+        cin >> choice;
+        return choice;
+}
+      
 
 
-int select_goat(list<Goat> trip);
+
+
+int select_goat(list<Goat> trip); // done
 void delete_goat(list<Goat> &trip);
-void add_goat(list<Goat> &trip, string [], string []);
-void display_trip(list<Goat> trip);
-int main_menu();
+void add_goat(list<Goat> &trip, string [], string []); // done
+void display_trip(list<Goat> trip); // done
+int main_menu(); // still needs work
 
 int main() {
     srand(time(0));
