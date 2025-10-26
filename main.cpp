d5902cfb3f4c829a700605ade5090f31f1d83182
Adam Yoonas <57850400+Kingadamy@users.noreply.github.com>
2025-10-25 17:56:54 -0700

    update

--- main.cpp
+++ main.cpp
@@ -7,6 +7,52 @@ using namespace std;
 
 const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
 
+class Goat {  
+private:
+    string name;
+    int age;
+    string color;
+public: 
+    Goat()                          { name = ""; age = 0; color = ""; }
+    // write three more constructors
+// first constructor will take only name
+    Goat (string n) {
+        name = n; age = 0; color = ""; 
+    }
+
+// second constructor will take name and age
+    Goat (string n, int a) {
+        name = n; age = a; color = ""; 
+    }
+
+// third constructor will take name, age, and color
+    Goat (string n, int a, string c) {
+        name = n; age = a; color = c; 
+    }
+
+    // setters and getters
+    void set_name(string n)         { name = n; };
+    string get_name() const         { return name; };
+    void set_age(int a)             { age = a; };
+    int get_age() const             { return age; }
+    void set_color(string c)        { color = c; }
+    string get_color() const        { return color; }
+
+    // write overloaded < operator for the std::list
+};
+
+
+// create main menu function for these options:
+// *** GOAT MANAGER 3001 ***
+// [1] Add a goat
+// [2] Delete a goat
+// [3] List goats
+// [4] Quit
+// Choice --> 
+
+
+
+
 int select_goat(list<Goat> trip);
 void delete_goat(list<Goat> &trip);
 void add_goat(list<Goat> &trip, string [], string []);
